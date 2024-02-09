fc113a8e7e65f7af68ba7be15135e6a0939be822
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-02-09 15:59:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,6 @@
 //Directories Finish
 
 int main() {
-//    system("echo 2744543@heM | sudo -S chmod 777 /sys/class/power_supply/BAT1/charge_control_end_threshold"); //okunurdan yazılıra geçirir
-//    system("echo 60 | tee /sys/class/power_supply/BAT1/charge_control_end_threshold"); //60 yapar
-//    system("cp /home/asus/battery_saver_asus_ubuntu/main /usr/bin"); //dosyayı kopyalıyor orjinal
-//    system("cp /home/asus/battery_saver_asus_ubuntu/main.desktop /home/asus/.config/autostart"); //dosyayı kopyalıyor orjinal
-
 
     //Change to permission (Writable-Readable-Changeable) power_ folder for BAT
     char *filename_password = PASSWORD_DIRECTORY;
